2df152d1bb45bcfc8dd9d697def7edaf3ba97bac
TNHao <trannhathao1516@gmail.com>
2022-03-21 00:08:11 +0700

    Initial commit, build basic layouts

---
+++ src/main/java/com/example/slangmaster/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.slangmaster;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
