e8402c5173264033e5f62919acd7faeab7789236
TNHao <trannhathao1516@gmail.com>
2022-03-26 16:07:55 +0700

    Added definition guessing game

--- src/main/java/com/example/Controllers/DefinitionGuessing/DefinitionGuessing.java
+++ src/main/java/com/example/Controllers/DefinitionGuessing/DefinitionGuessing.java
@@ -4,10 +4,13 @@ import com.example.Controllers.ScreenController.ScreenController;
 import com.example.SlangList.SlangList;
 import com.example.SlangWord.SlangWord;
 import com.example.Utils.Constant;
+import com.example.Utils.Toast;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -50,9 +53,50 @@ public class DefinitionGuessing implements Initializable {
         dLabel.setText(answers.get(3).getDefinition());
     }
 
+    public void showToast(Stage primaryStage, String mess, boolean status) {
+        int toastMsgTime = 1500;
+        int fadeInTime = 500;
+        int fadeOutTime = 500;
+        Toast.makeText(primaryStage, mess, toastMsgTime, fadeInTime, fadeOutTime, status);
+    }
+
+    public void checkAnswer(ActionEvent actionEvent) {
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        if (choosenIdx == answerIdx) {
+            score++;
+            showToast(primaryStage, "Yeah! Bạn đoán đúng rồi.", true);
+        } else {
+            score = 0;
+            showToast(primaryStage, "Oh no! Chúc bạn may mắn lần sau.", false);
+        }
+
+        scoreLabel.setText("Điểm: " + String.valueOf(score));
+        randomQuiz();
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         randomQuiz();
         scoreLabel.setText("Điểm: " + String.valueOf(score));
+    }
+
+    public void aClick(ActionEvent actionEvent) {
+        choosenIdx = 0;
+        checkAnswer(actionEvent);
+    }
+
+    public void bClick(ActionEvent actionEvent) {
+        choosenIdx = 1;
+        checkAnswer(actionEvent);
+    }
+
+    public void cClick(ActionEvent actionEvent) {
+        choosenIdx = 2;
+        checkAnswer(actionEvent);
+    }
+
+    public void dClick(ActionEvent actionEvent) {
+        choosenIdx = 3;
+        checkAnswer(actionEvent);
     }
 }
